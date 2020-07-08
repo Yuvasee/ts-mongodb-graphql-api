f4c61bf844511125e78f0f6b6feaaf80ef981ebb
Yuri Vasilchikov <yuriv@codevalue.net>
2020-07-08 12:51:45 +0300

    Base project configuration, logger

---
+++ src/service/logger.ts
@@ -0,0 +1,17 @@
+import winston = require("winston");
+
+const {
+	format: { combine, timestamp, json },
+} = winston;
+
+winston.configure({
+	format: combine(timestamp({ format: "YYYY-MM-DD HH:mm:ss" }), json()),
+	transports: [
+		new winston.transports.File({
+			filename: process.env.API_LOG_FILENAME,
+		}),
+		new winston.transports.Console({
+			format: winston.format.simple(),
+		}),
+	],
+});
